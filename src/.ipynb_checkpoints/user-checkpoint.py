de3cba6efde596bce21d5d20dc83213df77b6c20
Karan Samani <karan_samani@yahoo.co.in>
2019-10-18 12:11:35 +0100

    Changed the way of shuffling the dataframe, moved the train model function to be a user method called train. Same functionality maintianed. Stable

--- src/.ipynb_checkpoints/user-checkpoint.py
+++ src/.ipynb_checkpoints/user-checkpoint.py
@@ -8,7 +8,7 @@ class User:
     test_class, test_data):
         self._id = id
         self._model = model
-        self._history = None
+        self._history = []
         self._train_class = train_class
         self._train_data = train_data
         self._val_class = val_class
@@ -21,7 +21,62 @@ class User:
         self._pre_fit_loss = np.array([])
         self._pre_fit_accuracy = np.array([])
 
-        self._all_users = None
+    def evaluate_user(self, verbose = True):
+        """returns the loss and accuracy for the given User instance
+        on test data"""
+        test_data = self.get_test_data()
+        test_class = self.get_test_class()
+        model = self.get_model()
+        evaluation = model.evaluate(test_data,
+                                    test_class,
+                                    verbose = verbose)
+        return evaluation
+
+    def train(self, epochs=16, weights=None, verbose_fit = False,
+              verbose_evaluate = False):
+        # https://www.tensorflow.org/beta/tutorials/keras/basic_classification
+        # same seed value for consistency sake, across all trainings too
+        """
+        trains the model for the user
+        and updates the weights and history attribute for the user too
+        """
+
+        train_data = self.get_train_data()
+        train_class = self.get_train_class()
+        val_data = self.get_val_data()
+        val_class = self.get_val_class()
+        model = self.get_model()
+
+        if weights != None: # if provided, update model weights
+            model.set_weights(weights)
+
+        e = self.evaluate_user(verbose = verbose_evaluate)
+        self.add_pre_fit_evaluation(e)
+
+
+        history = model.fit(
+            train_data,
+            train_class,
+            epochs = epochs,
+            verbose = verbose_fit,
+            # batch_size = 2**8, #4k
+            # use_multiprocessing = True,
+            validation_data = (val_data, val_class)
+        )
+
+        e = self.evaluate_user(verbose = verbose_evaluate)
+        self.add_post_fit_evaluation(e)
+
+        # update user data
+        self.add_history(history)
+
+        return
+
+
+        
+        
+        
+        
     def get_data(self, ignore_first_n = 0, 
                  loss = False, accuracy = False, 
                  pre = False, post = False):
@@ -42,12 +97,6 @@ class User:
         if ignore_first_n > 0:
             data = data[ignore_first_n:]
         return data
-
-    def get_all_users(self):
-        return self._all_users
-    
-    def set_all_users(self, all_users):
-        self._all_users = all_users
 
     def get_pre_fit_loss(self):
         return self._pre_fit_loss
@@ -147,8 +196,8 @@ class User:
     def get_history(self):
         return self._history
 
-    def set_history(self, history):
-        self._history = history
+    def add_history(self, history):
+        self._history.append(history)
 
     def get_weights(self):
         return self.get_model().get_weights()
